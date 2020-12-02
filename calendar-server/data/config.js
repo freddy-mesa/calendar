f3f16105acb6a60782a8c44a607f4f3099750666
Freddy Mesa <mesa.freddy@gmail.com>
2020-12-02 12:50:35 -0400

    Change URI MongoAtlas

--- calendar-server/data/config.js
+++ calendar-server/data/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb+srv://admin:admin123@cluster0.wajwk.mongodb.net/calendar-events?retryWrites=true&w=majority"
+  url: "mongodb+srv://admin:<password>@cluster0.wajwk.mongodb.net/calendar-events?retryWrites=true&w=majority"
 };
